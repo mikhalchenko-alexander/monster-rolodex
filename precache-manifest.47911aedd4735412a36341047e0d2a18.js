26442630f9bdc447e7f2c1a3f0eb531dd6476e18
aam <aam@anahoret.com>
2020-05-14 16:10:30 +0300

    Updates

---
+++ precache-manifest.47911aedd4735412a36341047e0d2a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ae67879f6194cd680a98dc38a02944",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c157c1a31fd47e0061a7",
+    "url": "/monster-rolodex/static/css/main.e69801d3.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c157c1a31fd47e0061a7",
+    "url": "/monster-rolodex/static/js/main.b238b7bc.chunk.js"
+  },
+  {
+    "revision": "fdce5faec76031078f23",
+    "url": "/monster-rolodex/static/js/runtime-main.c9e8ecbd.js"
+  }
+]);
